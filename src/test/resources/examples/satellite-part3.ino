b123a1911fee74c5cb95ebf385677b072fdb9fbb
greg <gregc.sg@googlemail.com>
2017-01-29 21:55:28 +0000

    Add files for test and .gitattribute file
    - add a .gitattribute file to prevent ino line endings
      being changed and breaking local windows tests

---
+++ src/test/resources/examples/satellite-part3.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+pinMode( 4 , INPUT);
+pinMode( 2 , OUTPUT);
+}
+
+void loop()
+{
+if (digitalRead( 4))
+{
+digitalWrite( 2 , HIGH );
+delay( 1000 );
+digitalWrite( 2 , LOW );
+delay( 1000 );
+}
+else
+{
+digitalWrite( 2 , HIGH );
+}
+}
+
+
---
+++ src/test/resources/examples/satellite-part4.ino
@@ -0,0 +1,30 @@
+double tempIn = 0.0 ;
+double tempC = 0.0 ;
+
+void setup()
+{
+Serial.begin(9600);
+pinMode( 2 , OUTPUT);
+}
+
+void loop()
+{
+tempIn = analogRead(1) ;
+tempC = ( tempIn / 2 ) ;
+Serial.print(tempC);
+Serial.print(" ");
+Serial.println();
+if (( ( tempC ) > ( 25 ) ))
+{
+digitalWrite( 2 , HIGH );
+delay( 1000 );
+digitalWrite( 2 , LOW );
+delay( 1000 );
+}
+else
+{
+digitalWrite( 2 , HIGH );
+}
+}
+
+
