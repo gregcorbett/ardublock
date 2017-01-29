9de416a6b364587ac049ab97e9316b9286cc523d
greg <gregc.sg@googlemail.com>
2017-01-29 21:54:19 +0000

    Add comparisons between abp and ino files to test

--- src/test/java/com/ardublock/MainTest.java
+++ src/test/java/com/ardublock/MainTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Set;
+import java.util.Scanner;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -25,49 +26,65 @@ public class MainTest
 	private Main main;
 	private Translator translator;
 	private Context context;
-
+    
 	private void teardown() {
 		main.shutdown();
-	}
-
+    }
+    
 	@BeforeClass
 	public void setUp() throws SAXException, IOException, ParserConfigurationException
 	{
 		System.out.println("### setUp()");
 		main = new Main();
-		// main.startArdublock();
-		// context = Context.getContext();
-		// Workspace workspace = context.getWorkspaceController().getWorkspace();
-		// translator = new Translator(workspace);
-	}
+		main.startArdublock();
+		context = Context.getContext();
+		Workspace workspace = context.getWorkspaceController().getWorkspace();
+		translator = new Translator(workspace);
+    }
 	
 	@Test
 	public void testFiles() throws SAXException, IOException, ParserConfigurationException, SubroutineNameDuplicatedException, SocketNullException, SubroutineNotDeclaredException
 	{
 		System.out.println("### testFiles");
-		// String savedFiles[] = {
-		// 		"src/test/resources/examples/factorial.abp",
-		// 		"src/test/resources/examples/single-loop.abp",
-		// 		};
-		// 
-		// 
-		// 
-		// for (String savedFile : savedFiles)
-		// {
-		// 	translator.reset();
-		// 	File file = new File(savedFile);
-		// 	context.loadArduBlockFile(file);
-		// 	Set<RenderableBlock> loopBlockSet = translator.findEntryBlocks();
-		// 	Set<RenderableBlock> subroutineBlockSet = translator.findSubroutineBlocks();
-		// 	
-		// 	String code = translator.translate(loopBlockSet, subroutineBlockSet);
-		// 	System.out.println(code);
-		// }
-	}
-	
-	@Test
-	public void testSingleLoop() {
-		System.out.println("### testSingleLoop");
-		
-	}
+        String abpFiles[] = {
+            "src/test/resources/examples/satellite-part1.abp",
+            "src/test/resources/examples/satellite-part2.abp",
+            "src/test/resources/examples/satellite-part3.abp",
+            "src/test/resources/examples/satellite-part4.abp",
+        };
+
+        String inoFiles[] = {
+            "src/test/resources/examples/satellite-part1.ino",
+            "src/test/resources/examples/satellite-part2.ino",
+            "src/test/resources/examples/satellite-part3.ino",
+            "src/test/resources/examples/satellite-part4.ino",
+        };
+        
+        for (int i = 0; i < abpFiles.length; i++) {
+            String apbFile = abpFiles[i];
+            String inoFile = inoFiles[i];
+            translator.reset();
+            File file = new File(apbFile);
+            context.loadArduBlockFile(file);
+            Set<RenderableBlock> loopBlockSet = translator.findEntryBlocks();
+            Set<RenderableBlock> subroutineBlockSet = translator.findSubroutineBlocks();
+            
+            String generatedCode = translator.translate(loopBlockSet, subroutineBlockSet);
+            String expectedCode = new Scanner(new File(inoFile)).useDelimiter("\\Z").next();
+            
+            if (!generatedCode.equals(expectedCode)) {
+                System.out.println("Failing on " + apbFile);
+                System.out.println("======================================");
+                System.out.println(generatedCode);
+                System.out.println("======================================");
+                System.out.println(expectedCode);
+                System.exit(-1);
+            }
+            else {
+                System.out.println(apbFile + " passing");
+            }
+        }
+        
+    }
+    
 }
