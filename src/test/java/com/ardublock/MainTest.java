96e3c9a22fc1e61b59bf565744c73542ab234c06
greg <gregc.sg@googlemail.com>
2017-01-29 23:02:48 +0000

    Add sleep to reduce chance of concurrent access

--- src/test/java/com/ardublock/MainTest.java
+++ src/test/java/com/ardublock/MainTest.java
@@ -83,6 +83,13 @@ public class MainTest
             else {
                 System.out.println(apbFile + " passing");
             }
+
+            try {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace(); // To try and minimise concurrent access errors
+            }
         }
         
     }
